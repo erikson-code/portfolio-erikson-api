c445ccd31875de4b59f49ea89ca3ba38342075e8
erikson-code <erikson91249650@gmail.com>
2021-05-14 20:00:13 -0300

    .

--- config/prod.js
+++ config/prod.js
@@ -1,8 +1,8 @@
 module.exports = {
 
-    DB_URI: process.env.DB_URI
-    AUTH0_CLIENT: process.env.AUTH0_CLIENT
-    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE
-    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN
+    DB_URI: process.env.DB_URI,
+    AUTH0_CLIENT: process.env.AUTH0_CLIENT,
+    AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
+    AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
     
 }
